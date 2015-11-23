6bfba1368b18d76b1e594206ca69e16011c1a796
Adam Krieger <akrieger@obsglobal.com>
2015-11-22 21:57:42 -0600

    init

---
+++ ConcurrencySandbox/Areas/HelpPage/ModelDescriptions/ComplexTypeModelDescription.cs
@@ -0,0 +1,14 @@
+using System.Collections.ObjectModel;
+
+namespace ConcurrencySandbox.Areas.HelpPage.ModelDescriptions
+{
+    public class ComplexTypeModelDescription : ModelDescription
+    {
+        public ComplexTypeModelDescription()
+        {
+            Properties = new Collection<ParameterDescription>();
+        }
+
+        public Collection<ParameterDescription> Properties { get; private set; }
+    }
+}
---
+++ ConcurrencySandbox/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ConcurrencySandbox.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
