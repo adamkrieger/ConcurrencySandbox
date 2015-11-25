eb0b6456ae526be8719e6a5ee72bc92299c2e705
Adam Krieger <akrieger@obsglobal.com>
2015-11-24 20:47:01 -0600

    Silly -am command doesn't add the files.

---
+++ ConcurrencySandbox/Controllers/WaitController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Web.Http;
+using ConcurrencySandbox.Work;
+using Newtonsoft.Json;
+using NLog;
+
+namespace ConcurrencySandbox.Controllers
+{
+    public class WaitController : ApiController
+    {
+        public async Task<Dictionary<string, string>> Get([FromUri] int ms)
+        {
+            //Sanitize
+            ms = ms > 2000 ? 1 : ms;
+
+            var waiting = new Waiter();
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            stopwatch.Restart();
+            waiting.DoWork(ms);
+            var inOrderSpan = stopwatch.Elapsed;
+
+            stopwatch.Restart();
+            await waiting.DoWorkAsync(ms);
+            var asyncSpan = stopwatch.Elapsed;
+
+            var returnDictionary = new Dictionary<string, string>
+            {
+                {"processors", Environment.ProcessorCount.ToString()},
+                {"inOrderSpan", inOrderSpan.ToString()},
+                {"asyncSpan", asyncSpan.ToString()}
+            };
+
+            var results = JsonConvert.SerializeObject(returnDictionary);
+            LogManager.GetCurrentClassLogger().Log(LogLevel.Info, results);
+
+            return returnDictionary;
+        }
+    }
+}
---
+++ ConcurrencySandbox/Work/Waiter.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConcurrencySandbox.Work
+{
+    public class Waiter
+    {
+        public int DoWork(int ms)
+        {
+            var a = Wait(ms);
+
+            var b = Wait(ms);
+
+            return a + b;
+        }
+
+        public async Task<int> DoWorkAsync(int ms)
+        {
+            var a = Task.Run(() => Wait(ms));
+
+            var b = Task.Run(() => Wait(ms));
+
+            return await a + await b;
+        }
+
+        private static int Wait(int ms)
+        {
+            Thread.Sleep(ms);
+
+            return 21;
+        }
+    }
+}
